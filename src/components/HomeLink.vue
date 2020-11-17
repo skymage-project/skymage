46f1169511fb68fb0d9e3a7a17cccaa4521b5537
skanderKhabou <skander.khabou@gmail.com>
2020-11-17 10:44:04 +0100

    deleting the original container of app and hello world creating navbar and home link component

---
+++ src/components/HomeLink.vue
@@ -0,0 +1,10 @@
+// ./components/HomeLink.vue
+<script>
+export default {
+  name: "HomeLink",
+};
+</script>
+
+<template>
+  <a href="/">Accueil</a>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+
+    <!-- <div>
+        <header class="nav-opt-sprite nav-locate-us nav-lang-en nav-ssl nav-unrec">
+            <div class="container-fluid desktop-nav">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <div class="logo-area">
+                            <a href = "#">
+                                <img src="//" alt="logo" class="img-fluid">
+                            </a>              
+                    </div>
+                </div>
+    </div>
+</template> -->
+
+
+<!-- export default !;
+</script> -->
+
+// ./components/NavLink.vue
+<script>
+export default {
+  name: "NavBar",
+  props: ["url", "text"],
+};
+</script>
+
+<template>
+  <a :href="url">{{ text }}</a>
+</template>
