0c5aadcabcdf260a0c3817de2a01714182d3f1f6
selimsahnoun <sahnounselim@gmail.com>
2020-11-20 19:16:23 +0100

    catalog renders items with infos from the store

--- src/store/index.js
+++ src/store/index.js
@@ -4,84 +4,116 @@ import userAuth from '../service/user.service';
 Vue.use(Vuex);
 const user = JSON.parse(localStorage.getItem('user'));
 const initialState = user
-  ? { status: { loggedIn: true }, user }
-  : { status: { loggedIn: false }, user: null };
+	? { status: { loggedIn: true }, user }
+	: { status: { loggedIn: false }, user: null };
 export default new Vuex.Store({
-  state: {
-  //   drawer: false
-  // },
-  // mutations: {
-  //   SET_DRAWER: (state)=>{
-  //     state.drawer = !state.drawer;
-  //   }
-  // },
-  // actions: {
-  //   OPEN_DRAWER: (context)=>{
-  //     context.commit('SET_DRAWER');
+	state: {
+		initialState,
+		tricks: [],
+	},
+	mutations: {
+		FETCH_TRICKS: (state, payload) => {
+			state.tricks = payload;
+		},
 
+		loginSuccess(state, user) {
+			state.initialState.status.loggedIn = true;
+			state.initialState.user = user;
+		},
 
-    initialState,
+		loginFailure(state) {
+			state.initialState.status.loggedIn = false;
+			state.initialState.user = null;
+		},
 
-  },
-  mutations: {
-    loginSuccess(state, user) {
-      state.initialState.status.loggedIn = true;
-      state.initialState.user = user;
-    },
+		registerSuccess(state) {
+			state.initialState.status.loggedIn = false;
+		},
 
-    loginFailure(state) {
-      state.initialState.status.loggedIn = false;
-      state.initialState.user = null;
-    },
+		registerFailure(state) {
+			state.initialState.status.loggedIn = false;
+		},
 
-    registerSuccess(state) {
-      state.initialState.status.loggedIn = false;
-    },
+		logout(state) {
+			state.initialState.status.loggedIn = false;
+			state.initialState.user = null;
+		},
+	},
+	actions: {
+		fetchTricks({ commit }) {
+			var tricks = [
+				{
+					id: 1,
+					name: 'Borrowed Vape Through Bills ',
+					quickDescription:
+						'Object through bill effects are incredible, but they all have one flaw.',
+					urlVideos: 'https://j.gifs.com/oVxynX.gif',
+					urlPictures: 'https://j.gifs.com/oVxynX.gif',
+					price: '14,95$',
+					difficulty: 'Easy',
+					author: 'Alex',
+					description:
+						"With the VAPE THRUU gimmick, you'll be able to borrow both objects (JUUL & BILL) making it completely universal.",
+				},
+				{
+					id: 2,
+					name: 'TSA by Adam Wilber',
+					quickDescription:
+						'Object through bill effects are incredible, but they all have one flaw.',
+					urlVideos: 'https://j.gifs.com/oVxynX.gif',
+					urlPictures: 'https://j.gifs.com/vlG9AM.gif',
+					price: '225,95$',
+					difficulty: 'Hard',
+					author: 'Adam Wilber',
+					description: 'Vanish bag',
+				},
+				{
+					id: 2,
+					name: 'TSA by Adam Wilber',
+					quickDescription:
+						'Object through bill effects are incredible, but they all have one flaw.',
+					urlVideos: 'https://j.gifs.com/oVxynX.gif',
+					urlPictures: 'https://j.gifs.com/vlG9AM.gif',
+					price: '225,95$',
+					difficulty: 'Hard',
+					author: 'Adam Wilber',
+					description: 'Vanish bag',
+				},
+			];
+			commit('FETCH_TRICKS', tricks);
+		},
+		login({ commit }, user) {
+			return userAuth.login(user).then(
+				(user) => {
+					commit('loginSuccess', user);
+					return Promise.resolve(user);
+				},
+				(error) => {
+					commit('loginFailure');
+					return Promise.reject(error);
+				}
+			);
+		},
 
-    registerFailure(state) {
-      state.initialState.status.loggedIn = false;
-    },
-    
-    logout(state) {
-      state.initialState.status.loggedIn = false;
-      state.initialState.user = null;
-    },
-  },
-  actions: {
+		register({ commit }, user) {
+			console.log('user', user);
+			return userAuth.register(user).then(
+				(response) => {
+					commit('registerSuccess');
+					return Promise.resolve(response.data);
+				},
+				(error) => {
+					commit('registerFailure');
+					return Promise.reject(error);
+				}
+			);
+		},
 
-    login({ commit }, user) {
-      return userAuth.login(user).then(
-        user => {
-          commit('loginSuccess', user);
-          return Promise.resolve(user);
-        },
-        error => {
-          commit('loginFailure');
-          return Promise.reject(error);
-        }
-      );
-    },
+		logout({ commit }) {
+			userAuth.logout();
+			commit('logout');
+		},
+	},
 
-    register({ commit }, user) {
-      console.log('user', user);
-      return userAuth.register(user).then(
-        response => {
-          commit('registerSuccess');
-          return Promise.resolve(response.data);
-        },
-        error => {
-          commit('registerFailure');
-          return Promise.reject(error);
-        }
-      );
-    },
-
-    logout({ commit }) {
-      userAuth.logout();
-      commit('logout');
-    }
-  },
-
-  modules: {
-  },
+	modules: {},
 });
