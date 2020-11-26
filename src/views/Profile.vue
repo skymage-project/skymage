e8b785c27d204f5b8fdfe4dd352c8249f146e9e0
alasoui <alaasoui29@gmail.com>
2020-11-26 20:53:39 +0100

    working on user info

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,183 +1,45 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="1000" tile>
-      <v-img
-        height="300"
-        src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-      ></v-img>
-      <v-row style="margin: 2.5%; position: absolute; top: 230px">
-        <v-list-item>
-          <v-list-item-avatar size="100">
-            <img
-              src="https://www.w3schools.com/howto/img_avatar.png"
-              alt="user.firstName"/>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title class="title" style="margin-top: 20px">{{
-              user.firstName + " " + user.lastName
-            }}</v-list-item-title>
-            <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-row>
-    </v-card>
-    
-    <v-container fluid>
-        <v-layout column>
-            <v-card>
-                <v-card-text>
-                    <v-flex class="mb-4">
-                        <v-avatar size="96" class="mr-4">
-                            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar">
-                        </v-avatar>
-                        <v-btn @click="openAvatarPicker">Change Avatar</v-btn>
-                    </v-flex>
-                    <v-text-field
-                        v-model="user.firstName.firstName"
-                        label="FirstName"></v-text-field>
-                    <v-text-field
-                        v-model="user.lastName"
-                        label="Last Name"></v-text-field>
-                    <v-text-field
-                        v-model="user.email"
-                        label="Email Address"></v-text-field>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn color="primary" :loading="loading" @click.native="update">
-                        <v-icon left dark>check</v-icon>
-                        Save Changes
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-layout>
-        <avatar-picker
-            v-model="showAvatarPicker"
-            
-            @selected="selectAvatar"></avatar-picker>
-    </v-container>
-    <!-- <div>
-    <div class="page-header clear-filter" filter-color="orange">
-      <parallax
-        class="page-header-image"
-        style="background-image:url('img/bg5.jpg')"
+     <template>
+      <v-parallax
+        height="450"
+        src="https://www.abhayachauhan.com/wp-content/uploads/2017/10/magic-card-tricks.jpg"
+      ></v-parallax>
+      </template>
+    <v-fab-transition>
+      <v-btn
+        style="position: absolute; top: 600px; left: 50%"
+        color="orange"
+        fab
+        dark
+        absolute
+        bottom
+        left
       >
-      </parallax>
-      <div class="container">
-        <div class="photo-container">
-          <img src="img/ryan.jpg" alt="" />
-        </div>
-        <h3 class="title">Ryan Scheinder</h3>
-        <p class="category">Photographer</p>
-        <div class="content">
-          <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
-          </div>
-          <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
-          </div>
-          <div class="social-description">
-            <h2>48</h2>
-            <p>Bookmarks</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="section">
-      <div class="container">
-        <div class="button-container">
-          <a href="#button" class="btn btn-primary btn-round btn-lg">Follow</a>
-          <a
-            href="#button"
-            class="btn btn-default btn-round btn-lg btn-icon"
-            rel="tooltip"
-            title="Follow me on Twitter"
-          >
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a
-            href="#button"
-            class="btn btn-default btn-round btn-lg btn-icon"
-            rel="tooltip"
-            title="Follow me on Instagram"
-          >
-            <i class="fab fa-instagram"></i>
-          </a>
-        </div>
-        <h3 class="title">About me</h3>
-        <h5 class="description">
-          An artist of considerable range, Ryan — the name taken by
-          Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
-          records all of his own music, giving it a warm, intimate feel with a
-          solid groove structure. An artist of considerable range.
-        </h5>
-        <div class="row">
-          <div class="col-md-6 ml-auto mr-auto">
-            <h4 class="title text-center">My Portfolio</h4>
-          </div>
-          <tabs
-            pills
-            class="nav-align-center"
-            tab-content-classes="gallery"
-            tab-nav-classes="nav-pills-just-icons"
-            type="primary"
-          >
-            <tab-pane title="Profile">
-              <i slot="label" class="now-ui-icons design_image"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg6.jpg" class="img-raised" />
-                    <img src="img/bg11.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-
-            <tab-pane title="Home">
-              <i slot="label" class="now-ui-icons location_world"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg1.jpg" alt="" class="img-raised" />
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-
-            <tab-pane title="Messages">
-              <i slot="label" class="now-ui-icons sport_user-run"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg6.jpg" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-          </tabs>
-        </div>
-      </div>
-    </div>
-  </div> -->
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-fab-transition>
+    <v-avatar size="110" style="position: absolute; top: 440px; left: 45%">
+      <v-img src="https://www.w3schools.com/howto/img_avatar.png"></v-img>
+    </v-avatar>
+    <v-list-item-content>
+      <v-list-item-title
+        class="title"
+        style="
+          margin-top: 25px;
+          margin-left: 0.5cm;
+          color: orange;
+          text-align: center;
+        "
+      >
+        {{ user.firstName + " " + user.lastName }}</v-list-item-title
+      >
+      <v-list-item-subtitle
+        style="margin-left: 0.5cm; color: orange; text-align: center"
+        >{{ user.email }}</v-list-item-subtitle
+      >
+    </v-list-item-content>
+    <div style="margin-top:2000px">hzhj</div>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  }
 };
