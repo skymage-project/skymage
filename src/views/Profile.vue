d8207176c7688fbc3faef7855d478393a6405fb8
alasoui <alaasoui29@gmail.com>
2020-11-27 16:10:21 +0100

    make the upload show and hide on hover and navegate between thewishes and fatoura

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,69 +8,73 @@
     </div>
 
     <div id="profile">
-      <v-hover v-slot="{ hover }">
-        <v-avatar size="110" style="margin-top: -1.5cm">
-          <v-img src="https://www.w3schools.com/howto/img_avatar.png"></v-img>
-          <v-btn v-if="hover" color="orange" fab dark x-small absolute right>
+      <v-hover>
+        <div id="avatar-container" slot-scope="{ hover }">
+          <v-avatar size="110">
+            <v-img src="https://www.w3schools.com/howto/img_avatar.png"></v-img>
+          </v-avatar>
+          <v-btn id="uploads-btn" v-if="hover" fab dark x-small absolute>
             <v-icon>mdi-camera</v-icon>
           </v-btn>
-        </v-avatar>
+        </div>
       </v-hover>
+        <v-card height="300" width="400" style="border-radius: 8px;">
+          <v-row>
+            <v-col cols="6" sm="6">
+              <h2>Name:</h2>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <h4>{{ user.firstName + " " + user.lastName }}</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6" sm="6">
+              <h2>Date of birth:</h2>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <h4>{{ user.dateOfBirth }}</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6" sm="6">
+              <h2>Country:</h2>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <h4>{{ user.country }}</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6" sm="6">
+              <h2>Email:</h2>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <h4>{{ user.email }}</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6" sm="6">
+              <h2>Phone:</h2>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <h4>{{ user.phoneNumber }}</h4>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-bottom-navigation id="btnNav" v-model="value" dark shift grow>
+          <v-btn>
+            <span>Wishes</span>
 
-      <v-row>
-        <v-col cols="2" sm="2">
-          <h2>Name:</h2>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <h4>{{ user.firstName + " " + user.lastName }}</h4>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="2" sm="2">
-          <h2>Date of birth:</h2>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <h4>{{ user.dateOfBirth }}</h4>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="2" sm="2">
-          <h2>Country:</h2>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <h4>{{ user.country }}</h4>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="2" sm="2">
-          <h2>Email:</h2>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <h4>{{ user.email }}</h4>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="2" sm="2">
-          <h2>Phone:</h2>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <h4>{{ user.phoneNumber }}</h4>
-        </v-col>
-      </v-row>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
 
-      <v-bottom-navigation id="btnNav" v-model="value" dark shift>
-        <v-btn>
-          <span>Wishes</span>
+          <v-btn>
+            <span>Fatoura</span>
 
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn>
-          <span>Fatoura</span>
-
-          <v-icon> mdi-currency-usd</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
+            <v-icon> mdi-currency-usd</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+        <h1 v-if="value===0">im wishes</h1>
+        <h1 v-if="value===1">im fatoura</h1>
       <div style="margin-top: 2000px">hzhj</div>
     </div>
   </div>
@@ -79,7 +83,7 @@
 <script>
 export default {
   name: "Profile",
-
+  data: () => ({ value: 0 }),
   computed: {
     user() {
       return this.$store.state.initialState.user;
@@ -101,11 +105,23 @@ h2 {
   color: rgb(255, 255, 255);
 }
 h4 {
+  margin-top: 7px;
   color: orange;
 }
-#btnNav{
-   
-    background-color:orange;
-  
+#btnNav {
+  background-color: orange;
+}
+
+#avatar-container {
+  height: 109px;
+  margin-top: -1.5cm;
+  width: 109px;
+  border-radius: 50%;
+  display: inline-block;
+}
+
+#uploads-btn {
+  margin-top: -1cm;
+  background-color: orange;
 }
 </style>
