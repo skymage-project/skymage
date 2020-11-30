07965699197c1f520752be519d9648465580dfe9
alasoui <alaasoui29@gmail.com>
2020-11-30 18:25:44 +0100

    render the wish component with a real wish items add remove button if user want to remove item from wish list

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -29,6 +29,13 @@ export const mutations = {
 		}
 		!itemExist ? state.wishList.push(payload) : 0;
 	},
+	REMOVE_FROM_WISHES:(state, payload)=>{
+		for (var i = 0; i < state.wishList.length; i++) {
+			if(payload===state.wishList[i].id){
+				state.wishList.splice(i,1);
+			}
+		}
+	}
 };
 export const actions = {
 	addToCart({ commit }, payload) {
@@ -43,6 +50,9 @@ export const actions = {
 	addtToWishlist({ commit }, payload) {
 		commit('ADD_ITEM_TO_WISHLIST', payload);
 	},
+	removeFromWishes({ commit },payload) {
+		commit('REMOVE_FROM_WISHES', payload);
+	}
 };
 export const getters = {
 	cartItemsLength: (state) => {
