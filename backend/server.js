93d0872c06e45c7bb25613418d6ca879b617f2e5
selimsahnoun <sahnounselim@gmail.com>
2020-11-30 09:40:18 +0100

    cloudinay config

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database/index');
 const verifyToken = require('./middleware/verifyToken');
-<<<<<<< HEAD
 const cors = require('cors');
 const helmet = require('helmet');
 var morgan = require('morgan');
 const user = require('./routes/user');
 const items = require('./routes/items');
-=======
-const cors = require("cors");
-const helmet = require("helmet");
-var morgan = require('morgan');
-const user =require('./routes/user');
->>>>>>> a92de550e4c99e99b930c210fcb758f81d0ff2ad
 const app = express();
 
 app.use(cors());
