abd95d8e6da61ff67ef0d17ea1bb669c1ff93d02
skanderKhabou <skander.khabou@gmail.com>
2020-12-01 23:21:15 +0100

    add the connection with the cart in the trick over view some trick over view some issue to fix in the getters

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,123 +1,123 @@
 <template>
-	<v-card
-		class="mx-auto"
-		width="23%"
-		height="380px"
-		@mouseover="toggleHover"
-		@mouseout="toggleHover"
-	>
-		<v-img height="150px" :src="trick.urlPictures[0].urlPictures">
-			<v-btn icon @click="addtToWishlist">
-				<v-icon>mdi-heart-outline</v-icon>
-			</v-btn>
-		</v-img>
-		<v-card-text class="pt-4" style="position:relative;">
-			<v-btn
-				absolute
-				color="orange"
-				class="white--text"
-				fab
-				small
-				right
-				top
-				@click="addToCart"
-			>
-				<v-icon>mdi-cart</v-icon>
-			</v-btn>
-		</v-card-text>
-		<v-card-title>
-			<!-- NameTrick -->
-			{{
-				trick.name.length > 20 ? trick.name.slice(0, 30) + '...' : trick.name
-			}}
-		</v-card-title>
-		<v-card-subtitle>
-			<!-- category  -->
-			{{ trick.category }}
-		</v-card-subtitle>
-		<v-card-subtitle>
-			<!-- price  -->
-			{{ trick.price }}$
-		</v-card-subtitle>
+  <v-card
+    class="mx-auto"
+    width="23%"
+    height="380px"
+    @mouseover="toggleHover"
+    @mouseout="toggleHover"
+  >
+    <v-img height="150px" :src="trick.urlPictures[0].urlPictures">
+      <v-btn icon @click="addtToWishlist">
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+    </v-img>
+    <v-card-text class="pt-4" style="position:relative;">
+      <v-btn
+        absolute
+        color="orange"
+        class="white--text"
+        fab
+        small
+        right
+        top
+        @click="addToCart"
+      >
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-card-text>
+    <v-card-title>
+      <!-- NameTrick -->
+      {{
+        trick.name.length > 20 ? trick.name.slice(0, 30) + "..." : trick.name
+      }}
+    </v-card-title>
+    <v-card-subtitle>
+      <!-- category  -->
+      {{ trick.category }}
+    </v-card-subtitle>
+    <v-card-subtitle>
+      <!-- price  -->
+      {{ trick.price }}$
+    </v-card-subtitle>
 
-		<v-card-actions transition="fade-transition">
-			<v-btn color="orange lighten-2" text> Read More </v-btn>
-	<v-btn color="orange " v-show="hover" @click="showD">Quick View</v-btn>
+    <v-card-actions transition="fade-transition">
+      <v-btn color="orange lighten-2" text> Read More </v-btn>
+      <v-btn color="orange " v-show="hover" @click="showD">Quick View</v-btn>
 
-        <TrickOverView
-          :trick="trick"
-          :showDiv="showDiv"
-          @toggle-over-view="showD"
-        />
-			<v-spacer></v-spacer>
+      <TrickOverView
+        :trick="trick"
+        :showDiv="showDiv"
+        @toggle-over-view="showD"
+      />
+      <v-spacer></v-spacer>
 
-			<v-btn icon @click="showByIndex">
-				<v-icon :id="trick.id">
-					{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon
-				>
-			</v-btn>
-		</v-card-actions>
+      <v-btn icon @click="showByIndex">
+        <v-icon :id="trick.id">
+          {{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon
+        >
+      </v-btn>
+    </v-card-actions>
 
-		<v-expand-transition>
-			<div v-if="show">
-				<v-divider></v-divider>
+    <v-expand-transition>
+      <div v-if="show">
+        <v-divider></v-divider>
 
-				<v-card-text>
-					<!-- description of the trick -->
-					{{ trick.description }}
-				</v-card-text>
-			</div>
-		</v-expand-transition>
-	</v-card>
+        <v-card-text>
+          <!-- description of the trick -->
+          {{ trick.description }}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
 import TrickOverView from "./TrickOverView";
 export default {
-components: { TrickOverView },
-	props: ['trick'],
-	data() {
-		return {
-			show: false,
-			hover: false,
-			showDiv: false,
-		};
-	},
-	methods: {
-		showByIndex(e) {
-			this.show[e.target.id] = !this.show[e.target.id];
-			this.key++;
-		},
-		addToCart() {
-			let item = {
-				id: this.trick.id,
-				name: this.trick.name,
-				picture: this.trick.urlPictures[0].urlPictures,
-				price: this.trick.price,
-				quantity: 1,
-			};
-			this.$store.dispatch('addToCart', item);
-		},
-		toggleHover() {
-			this.hover = !this.hover;
-		},
-		showByIndex(e) {
-			this.show = !this.show;
-		},
-		addtToWishlist() {
-			let item = {
-				id: this.trick.id,
-				name: this.trick.name,
-				picture: this.trick.urlPictures[0].urlPictures,
-				price: this.trick.price,
-				quantity: 1,
-			};
-			this.$store.dispatch('addtToWishlist', item);
-		},
-		showD() {
+  components: { TrickOverView },
+  props: ["trick"],
+  data() {
+    return {
+      show: false,
+      hover: false,
+      showDiv: false,
+    };
+  },
+  methods: {
+    showByIndex(e) {
+      this.show[e.target.id] = !this.show[e.target.id];
+      this.key++;
+    },
+    addToCart() {
+      let item = {
+        id: this.trick.id,
+        name: this.trick.name,
+        picture: this.trick.urlPictures[0].urlPictures,
+        price: this.trick.price,
+        quantity: 1,
+      };
+      this.$store.dispatch("addToCart", item);
+    },
+    toggleHover() {
+      this.hover = !this.hover;
+    },
+    showByIndex(e) {
+      this.show = !this.show;
+    },
+    addtToWishlist() {
+      let item = {
+        id: this.trick.id,
+        name: this.trick.name,
+        picture: this.trick.urlPictures[0].urlPictures,
+        price: this.trick.price,
+        quantity: 1,
+      };
+      this.$store.dispatch("addtToWishlist", item);
+    },
+    showD() {
       this.showDiv = !this.showDiv;
     },
-	},
+  },
 };
 </script>
 
--- src/components/TrickOverView.vue
+++ src/components/TrickOverView.vue
@@ -14,9 +14,9 @@
                 <v-col cols="12" sm="12" md="12">
                   <v-carousel style="height:370px">
                     <v-carousel-item
-                      v-for="item in items"
+                      v-for="item in trick.urlPictures"
                       :key="item.id"
-                      v-html="item.content"
+                      v-html="getiframe(item.urlPictures)"
                       reverse-transition="fade-transition"
                       transition="fade-transition"
                       style="height:370px"
@@ -25,21 +25,23 @@
                 <v-col cols="12" sm="12" md="12">
                   <v-layout align-start justify-end fill-height>
                     <v-card
-                      v-for="item in items"
+                      v-for="item in trick.urlPictures"
                       :key="item.id"
                       class="pa-2"
                       width="25%"
                     >
-                      <v-img
+                      <!-- <v-img
                         v-model="selected"
                         @click="bindImg"
                         height="120px"
                         v-html="item.content"
+                      /> -->
+                      <v-img
+                        height="120px"
+                        v-html="getiframeDown(item.urlPictures)"
                       />
-                      <!-- <v-img height="150px" :src="trick.urlPictures[0].urlPictures" /> -->
-                    </v-card>
-                  </v-layout></v-col
-                >
+                    </v-card> </v-layout
+                ></v-col>
               </v-row>
             </v-card>
           </v-col>
@@ -71,14 +73,40 @@
                   <v-col cols="5"
                     ><h1 class="price">{{ trick.price }} $</h1></v-col
                   >
-                  <v-col cols="12"></v-col>
-                  <v-col cols="12"></v-col>
+
+                  <v-col cols="2"
+                    ><v-btn icon class="plus-cart-trick" @click="addItemToCart">
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn></v-col
+                  >
+                  <v-col cols="2">
+                    <v-btn icon>
+                      <v-icon
+                        >mdi-numeric-{{
+                          getItemQuantity(trick.id)
+                        }}-circle-outline</v-icon
+                      >
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="2"
+                    ><v-btn icon @click="decreaseQuantity">
+                      <v-icon>mdi-minus</v-icon>
+                    </v-btn></v-col
+                  >
+                  <v-col cols="3"
+                    ><v-btn icon @click="removeItemFromCart">
+                      <v-icon>mdi-delete-outline</v-icon>
+                    </v-btn></v-col
+                  >
                   <v-col cols="6"></v-col>
                   <v-col cols="6"
                     ><v-btn
                       color="orange lighten-2"
                       class="buttonV"
-                      @click="toggleOverView"
+                      @click="
+                        addItemToCart();
+                        toggleOverView();
+                      "
                       >Add To Cart</v-btn
                     ></v-col
                   >
@@ -94,34 +122,39 @@
   </v-layout>
 </template>
 <script>
+import { mapState, mapGetters } from "vuex";
 export default {
   props: ["showDiv", "trick"],
   name: "TrickOverView",
+  computed: {
+    ...mapState(["displayedTricks", "cart"]),
+    ...mapGetters(["getItemQuantity"]),
+  },
   data() {
     return {
       show: false,
-      items: [
-        {
-          id: "1",
-          content:
-            '<img width="640" height="370" src="https://i.pinimg.com/originals/a7/55/a9/a755a93ecc3605830526040c842d7d71.jpg"/>',
-        },
-        {
-          id: "2",
-          content:
-            '<img width="640" height="370" src="https://d8jyfye76ik7i.cloudfront.net/gallery/blog/thumbs/whats-the-difference-between-bicycle-rider-back-and-standard-decks-of-cards.jpg"/>',
-        },
-        {
-          id: "3",
-          content:
-            '<img width="640" height="370" src="https://i.ebayimg.com/images/g/xoMAAOSwUlxfVXFc/s-l1600.jpg"/>',
-        },
-        {
-          id: "4",
-          content:
-            '<iframe width="640" height="370" src="https://www.youtube.com/embed/3lfQIK9hN7k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        },
-      ],
+      // items: [
+      //   {
+      //     id: "1",
+      //     content:
+      //       '<img width="640" height="370" src="https://i.pinimg.com/originals/a7/55/a9/a755a93ecc3605830526040c842d7d71.jpg"/>',
+      //   },
+      //   {
+      //     id: "2",
+      //     content:
+      //       '<img width="640" height="370" src="https://d8jyfye76ik7i.cloudfront.net/gallery/blog/thumbs/whats-the-difference-between-bicycle-rider-back-and-standard-decks-of-cards.jpg"/>',
+      //   },
+      //   {
+      //     id: "3",
+      //     content:
+      //       '<img width="640" height="370" src="https://i.ebayimg.com/images/g/xoMAAOSwUlxfVXFc/s-l1600.jpg"/>',
+      //   },
+      //   {
+      //     id: "4",
+      //     content:
+      //       '<iframe width="640" height="370" src="https://www.youtube.com/embed/3lfQIK9hN7k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      //   },
+      // ],
     };
   },
   methods: {
@@ -129,6 +162,41 @@ export default {
       this.$emit("toggle-over-view");
     },
     bindImg() {},
+    addItemToCart() {
+      let quantity = this.trick.quantity + 1;
+      let item = {
+        id: this.trick.id,
+        name: this.trick.name,
+        picture: this.trick.picture,
+        price: this.trick.price,
+        quantity: quantity,
+      };
+      this.$store.dispatch("addToCart", item);
+    },
+    decreaseQuantity() {
+      let quantity = this.trick.quantity - 1;
+      let item = {
+        id: this.trick.id,
+        name: this.trick.name,
+        picture: this.trick.picture,
+        price: this.trick.price,
+        quantity: quantity,
+      };
+      this.$store.dispatch("decreaseQuantity", item);
+    },
+    removeItemFromCart() {
+      this.$store.dispatch("removeFromCart", this.trick.id);
+    },
+    getiframe(url) {
+      if (url.includes("www.youtube.com")) {
+        return `<iframe width="640" height="370" src="${url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      } else {
+        return `<img width="600" height="370" src="${url}"/>`;
+      }
+    },
+    getiframeDown(url) {
+      return `<img width="120px" height="120px" src="${url}"/>`;
+    },
   },
 };
 </script>
